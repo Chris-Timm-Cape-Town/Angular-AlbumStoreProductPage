7d608e32a6a588dffafabb5205864e1c1146b563
christopher timm <chris_timm@LIVE.COM>
2018-03-14 09:48:12 +0200

    wip

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http'
+import { Http, Response } from '@angular/http';
+
+import  'rxjs/add/operator/map';
 
 @Injectable()
 export class ProductService {
@@ -7,7 +9,8 @@ export class ProductService {
   private _albumUrl = '../assets/album.json';
   constructor(private _http: Http) { }
 
-  // getAlbum(id number) {
-
-  // } 
+  getAlbum(id: number) {
+    return this._http.get(this._albumUrl).map((response) =>
+    response.json());
+  } 
 }
